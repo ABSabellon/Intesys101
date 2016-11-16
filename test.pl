963a9108be78a4415ad44f060ad24e0a23d40f12
Aileen Sabellon <Aileen Sabellon>
2016-11-16 22:48:06 +0800

    plsssss pumasa sana ng prolog

--- test.pl
+++ test.pl
@@ -8,38 +8,46 @@ begin():-
 	write('You will be asked a couple of questions'),nl,
 	write('about your patient, to identify'),nl,
 	write(' if he/she has Appendicitis or Cholecystitis'),nl,
-	write('Please Enter Patient Name'),
-	read(NAME),
+	write('Please Enter Patient Name: '),
+	read(name),
 	write('Let us Start'),
-	ask(NAME).
+	ask(name).
 
 ask(name):-
 	write('What is your age?'),nl,
-	read(age),
+	read(name, age),
 	write('Does the patient experience pain?'),nl,
-	read(ans1),
-	write('You will be asked a couple of questions'),nl,
-	write('about your patient, to identify'),nl,
-	write(' if he/she has Appendicitis or Cholecystitis'),nl,
-	write('Please Enter Patient Name').
+	read(name, ans1),
+	write('Is the pain progressive?'),nl,
+	read(name, ans2),
+	write('Is the pain shifting?'),nl,
+	read(name, ans3),
+	write('Is the pain localized?'),nl,
+	read(name, ans4).
 
-hasPain(ans1):-
+hasPain(name, ans1):-
 	pain(ans1).
 
-symptomPain_appendicitis(X):-
-	isProgressive(Y),
-	isChildren(X),
-	isShifting(Y),
-	isLocalized(Y).
+symptomPain_appendicitis(name, ans2, age, ans3, ans4):-
+	isProgressive(ans2),
+	isChild(age),
+	isShifting(ans3),
+	isLocalized(ans4).
 
-symptomPain_appendicitis(X):-
-	isProgressive(Y),
-	isAdult(X),
-	isShifting(Y),
-	isLocalized(Y).
+symptomPain_appendicitis(ans2, age, ans3, ans4):-
+	isProgressive(ans2),
+	isAdult(age),
+	isShifting(ans3),
+	isLocalized(ans4).
 
-symptomPain_appendicitis(X):-
-	isProgressive(Y),
-	isAdult(X),
-	isLocalized(Y).
+symptomPain_appendicitis(ans2, age, ans4):-
+	isProgressive(ans2),
+	isAdult(age),
+	isLocalized(ans4).
+
+
+
+
+
+
 
